bbc3680ed4613121748720513bcc3f02d9f6a051
Sinchana P <83894491+sinchana-P@users.noreply.github.com>
2023-01-14 09:21:21 +0530

    mini project from scratch

---
+++ co38a4834950b81f65b4187b7/index.js
@@ -0,0 +1,58 @@
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+
+
+
+/**
+Challenge: Project setup
+
+- Create a `components` folder
+- Create the following components in separate files inside
+  the components folder.  In each one, just render an `h1` 
+  with the name of the component (e.g. return <h1>Navbar goes here</h1>):
+    - Navbar
+    - Main
+- Create an App component outside the components folder (sibling to
+  the index.js file)
+    - Have App render the Navbar and Main components
+- Import and render the App component inside of index.js using ReactDOM
+    - At this point you should have your "Navbar goes here" etc. showing up
+      in the mini-browser.
+- Go to Google fonts and get the "Inter" font with weights 400, 600, and 700.
+  Put the links to those fonts ABOVE the style.css link in index.html (Use
+  the `<link/>` elements instead of the @import or npm options for getting
+  the fonts. You may need to do some extra research to figure out how this 
+  works if you haven't done it before)
+*/
+
+
+import Navbar from "./components/Navbar"
+
+function Main() {
+  return (
+    <main>
+        <h1 className="main--title">Fun facts about React</h1>
+        <ul className="main--facts">
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </main>
+)
+}
+
+function App(){
+  return(
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
